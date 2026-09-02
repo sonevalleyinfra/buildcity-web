27968f932e404456ed40817cf34dd4020a03a8d5
Dhiru2 <singhdhiru.ds@gmail.com>
2026-09-02 12:45:00 +0530

    fix: addresses page me useMemo import add kiya

--- src/pages/customer/Addresses.jsx
+++ src/pages/customer/Addresses.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Navbar from "../../components/Navbar";
 import BottomNav from "../../components/BottomNav";
 import { useAddresses } from "../../context/AddressContext";
